be5e37ad22691d6015d14312661381b5df416e21
Yakash goyal <yakashgoyal123@gmail.com>
2025-04-03 23:38:14 +0530

    Prim's Algo

--- Prim's Algorithms/Prims.cpp
+++ Prim's Algorithms/Prims.cpp
@@ -1,73 +1,71 @@
-#include<iostream>
+#include <iostream>
+#include <climits>
 using namespace std;
-// void Prim(int e, int cost, int n){}
-pair<int,int> minimumWeightWhole(int n,int graph[100][100]){
-    int min;
-    pair<int,int> edge;
-    cout << "The minimum weighted edge is " << endl;
-    // min = graph[0][1];
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < n;j++){
-            if(graph[i][j] < min && graph[i][j] > 0){
-                // min = graph[i][j];
-                edge.first = i;
-                edge.second = j;
+#define INF INT_MAX
+#define MAX 100
+pair<int, int> minimumWeightWhole(int n, int graph[MAX][MAX]) {
+    int min = INF;
+    pair<int, int> edge = {-1, -1};
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (graph[i][j] < min && graph[i][j] > 0) {
+                min = graph[i][j];
+                edge = {i, j};
             }
         }
     }
     return edge;
 }
 
-pair<int,int> minimumWeight(int n,int arr){
-    int min;
-    pair<int,int> edge;
-    // cout << "The minimum weighted edge is " << endl;
-    // min = graph[0][1];
-    for(int i=0;i<n;i++){
-        
+void primMST(int n, int graph[MAX][MAX]) {
+    int parent[MAX],key[MAX];
+    bool inMST[MAX];
+    for (int i = 0; i < n; i++) {
+        key[i] = INF;
+        inMST[i] = false;
     }
-    return edge;
+    pair<int, int> edge = minimumWeightWhole(n, graph);
+    int start = edge.first;
+    key[start] = 0;
+    parent[start] = -1;
+    for (int count = 0; count < n - 1; count++){
+        int u = -1, minKey = INF;
+        for (int v = 0; v < n; v++) {
+            if (!inMST[v] && key[v] < minKey){
+                minKey = key[v];
+                u = v;
+            }
+        }
+        inMST[u] = true;
+        for (int v = 0; v < n; v++) {
+            if (graph[u][v] && !inMST[v] && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    cout << "Minimum Spanning Tree Edges:\n";
+    for (int i = 1; i < n; i++) {
+        cout << parent[i] << " - " << i << " : " << graph[i][parent[i]] << endl;
+    }
 }
-int main(){
+
+int main() {
     int n;
-    cout << "Enter the number of vertices in the graph" << endl;
+    cout << "Enter the number of vertices in the graph: ";
     cin >> n;
-    int graph[n][n],min;
-    for(int i = 0;i < n;i++){
-        cout << "Enter the edges weight for " << i << " vertex in order" << endl;
-        for(int j = 0;j < n;j++){
-            if(i == j)
-            {
-                graph[i][i] = 0;
-                
-            }
-            else
-                cin >> graph[i][j];
-                if(graph[i][j] == -1){
-                    graph[i][j] = INT64_MAX;
-                }
+    int graph[MAX][MAX];
+    cout << "Enter the adjacency matrix (-1 for no edge):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> graph[i][j];
+            if (graph[i][j] == -1)
+                graph[i][j] = INF;
+            if (i == j)
+                graph[i][j] = 0;
         }
     }
-    pair<int,int> edge = minimumWeightWhole(n,graph);
-    int near[n];
-    for(int i = 0;i < n; i++){
-        if(graph[i][edge.first] > graph[i][edge.second])
-        {
-            near[i] = edge.second;
-        }
-        else{
-            near[i] = edge.first;
-        }
-    }
-    near[edge.first] = 0;
-    near[edge.second] = 0;
-    min = graph[edge.first][edge.second];
-    int result[n][2];
-    result[0][0] = edge.first;
-    result[0][1] = edge.second;
-    for(int j=2;j < n;j++){
-        for(int i = 0;i < n;i++){
-
-        }
-    }
+    primMST(n, graph);
 }
